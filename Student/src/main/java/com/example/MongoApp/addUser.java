024675d8ba025dd6148d1f536f8e777baa5c2afa
Varatharajan <Sivabalen.Varatharajan@nielseniq.com>
2023-03-11 02:14:51 +0530

    Preview version(Without search/delete/update using certain category)

--- Student/src/main/java/com/example/MongoApp/addUser.java
+++ Student/src/main/java/com/example/MongoApp/addUser.java
@@ -1,26 +1,23 @@
 package com.example.MongoApp;
 
-import java.io.IOException;
 import java.rmi.ServerException;
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.MongoApp.model.student;
 import com.example.MongoApp.model.userRepo;
-
-import jakarta.servlet.http.HttpServletResponse;
-import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 public class addUser {
@@ -34,13 +31,17 @@ public class addUser {
 		response.sendRedirect("/swagger-ui.html");
 	}*/
 	
+	//Displaying all the records from the student database
+	
 	@GetMapping("/getalluser")
 	public List<student> getAll(){
 		if(repo.findAll().isEmpty())
 			System.out.println("The student database is empty");
 		return repo.findAll();
 	}
-		
+	
+	//Adding a student to the database
+	
 	@PostMapping("/addUser")
 	public ResponseEntity<student> create(@RequestBody student newJoinee) throws ServerException {
 		student newStudent = repo.save(newJoinee);
@@ -51,23 +52,33 @@ public class addUser {
 	    }
 	}
 	
+	//Updating student information
 	
 	@PutMapping("/updateUser")
-	public student updateStudent(@RequestParam(required = true) String id, @RequestParam(required = false) Integer age,@RequestParam(required = false) String name,@RequestParam(required = false) String department,@RequestParam(required = false) String city)
+	public student updateStudent(@RequestBody student s) throws ServerException
 	{
-		student s = repo.findById(city).orElseThrow(()->new IllegalStateException());
-		if(name != null)
-			s.setName(name);
-		System.out.println(age);
-		if(age>0)
-			s.setAge(age);
-		if(department == null)
-			s.setDepartment(department);
-		if(city == null)
-			s.setCity(city);
+		student modifyStudent = repo.findById(s.getId()).orElse(null);
 		
-		repo.save(s);
-		return s;
+		modifyStudent.setAge(s.getAge());
+		modifyStudent.setDepartment(s.getDepartment());
+		modifyStudent.setCity(s.getCity());
+		modifyStudent.setName(s.getName());
+		repo.save(modifyStudent);
+		return modifyStudent;	
+	}
+	
+	//Searching Student by using pathvariable
+	
+	@GetMapping("/searchUserById/{id}")
+	public student searchById(@PathVariable("id") String id) {
+		return repo.findById(id).get();
+	}
+
+	//Deleting Student from the database
+	
+	@DeleteMapping("/deleteUserById")
+	public void deleteUserById(@RequestParam("id") String id) {
+		repo.deleteById(id);
 		
 	}
 }
